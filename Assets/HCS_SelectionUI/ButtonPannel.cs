eb67c90f16a4db1b79558450ff05d46241adc664
Sagitarian812 <62075331+Sagitarian812@users.noreply.github.com>
2020-11-02 16:33:23 -0500

    Finished Networking stuff

---
+++ Assets/HCS_SelectionUI/ButtonPannel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAPI;
+
+public class ButtonPannel : MonoBehaviour
+{
+    public GameObject Pannel;
+
+    void Start()
+    {
+
+        Pannel.SetActive(true);
+    }
+
+
+    public void StartasHost()
+    {
+        NetworkingManager.Singleton.StartHost();
+        Pannel.SetActive(false);
+    }
+
+    public void StartasClient()
+    {
+        NetworkingManager.Singleton.StartClient();
+        Pannel.SetActive(false);
+    }
+
+
+    public void StartasServer()
+    {
+        NetworkingManager.Singleton.StartServer();
+        Pannel.SetActive(false);
+    }
+
+
+   
+   
+}
