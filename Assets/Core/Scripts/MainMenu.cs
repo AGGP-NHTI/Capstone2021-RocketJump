b0f7c107e13330e4861dd8fc36b7e5ee5bdb3962
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-11-02 16:39:22 -0500

    title anims and text mesh pro

---
+++ Assets/Core/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject settings;
+    public GameObject games;
+    
+    
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void Back()
+    {
+        main.SetActive(true);
+        settings.SetActive(false);
+        games.SetActive(false);
+    }
+    public void ToSettings()
+    {
+        main.SetActive(false);
+        settings.SetActive(true);
+    }
+    public void ToGames()
+    {
+        main.SetActive(false);        
+        games.SetActive(true);
+    }
+}
