bda7d729adb026e79a3a0cf74d28a338df1e44b1
hucota7 <hucota7@gmail.com>
2020-11-04 16:07:32 -0500

    imported sprite package; speedometer testing, not yet hooked up but functional if done manually

---
+++ Assets/Core/Scripts/SpeedometerUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedometerUI : MonoBehaviour
+{
+	public PlayerController pc;
+	public Image imgSpeedometer;
+
+	float velocity;
+
+	private void FixedUpdate()
+	{
+		if (pc == null) return;
+		imgSpeedometer.fillAmount = Mathf.SmoothDamp(imgSpeedometer.fillAmount, pc.GetSpeedometer(), ref velocity, 0.1f);
+	}
+}
