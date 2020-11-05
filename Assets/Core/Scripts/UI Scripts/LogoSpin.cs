780ace16172b6f8ee9e78859d15223fb6aa63ac8
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-11-05 16:46:34 -0500

    menu stuff

---
+++ Assets/Core/Scripts/UI Scripts/LogoSpin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoSpin : MonoBehaviour
+{
+    public GameObject centerpoint;
+    public GameObject offset;
+    public float speed;
+    public float distancex;
+    public float distancey;
+    Vector3 MoveDirection;
+    
+    void Start()
+    {
+        offset.transform.position = new Vector3(centerpoint.transform.position.x + distancex, centerpoint.transform.position.y, centerpoint.transform.position.z);
+    }
+
+    
+    void FixedUpdate()
+    {
+        centerpoint.transform.Rotate(speed * Time.fixedDeltaTime * Vector3.forward);
+        MoveDirection = (offset.gameObject.transform.position - transform.position).normalized;
+        gameObject.transform.up = MoveDirection;
+    }
+}
