fea808f9f7bcd9aca17e208568ecb5efca000221
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-01-26 16:40:13 -0500

    ui stuff

--- Assets/Core/Scripts/UI Scripts/SpeedometerScript.cs
+++ Assets/Core/Scripts/UI Scripts/SpeedometerScript.cs
@@ -5,24 +5,28 @@ using UnityEngine.UI;
 
 public class SpeedometerScript : MonoBehaviour
 {
-
     //Obviously the art aspects of the speedometer still need to be made, however the actual functionality is already made with this script
     //The current needle is just a placeholder
 
     public Image speedometer; // the actual image for the speedometer needle
     public Image background;
+
     private Transform speed_trans; // the images transform
-    private float max_rotation; // the maximum rotation the needle can have (max speed)
-    private float min_rotation; // the minumum rotation the needle can have (no speed)
-    public PlayerController player;
-    private bool playerLoaded;
+
+    private float max_rotation = 190; // the maximum rotation the needle can have (max speed)
+    private float min_rotation = 0; // the minumum rotation the needle can have (no speed)
 
     public float speed; // current speed
     public float maxSpeed; // max speed;
-
     public float speedPerc; // the percentage of speed on its way to max speed;
 
     float velocity;
+
+    public PlayerController player;
+
+    private bool playerLoaded = false;
+
+    Rigidbody rb;
 
     // ((value - min) / (max - min)) * 100 = percentage;
     // -((min * perc) / 100) + min + ((max * perc) / 100) = value;
@@ -30,10 +34,11 @@ public class SpeedometerScript : MonoBehaviour
     void Start()
     {
         speed_trans = speedometer.transform;
-        max_rotation = 190; // dont mess with
-        min_rotation = 0; // leave at 0
-        playerLoaded = false;
+        //max_rotation = 190; // dont mess with
+        //min_rotation = 0; // leave at 0
+        //playerLoaded = false;
         player = gameObject.transform.root.GetComponent<PlayerController>();
+        rb = player.GetComponent<Rigidbody>();
 
         if(!player)
         {
@@ -56,9 +61,10 @@ public class SpeedometerScript : MonoBehaviour
         }
 
         //speed = player.GetSpeedometer();
-        speed = player.GetComponent<Rigidbody>().velocity.magnitude;
+        speed = rb.velocity.magnitude;
 
-        speedPerc = ((speed - 0) / (maxSpeed - 0)) * 100; // get the speed percentage
+        //speedPerc = ((speed - 0) / (maxSpeed - 0)) * 100; // get the speed percentage
+        speedPerc = (speed / maxSpeed) * 100; // get the speed percentage
 
         if(speedPerc > 100) { speedPerc = 100; } // limit testing
         if(speedPerc < 0) { speedPerc = 0; } // limit testing
