76d050625ed261721953a1e1f06ae261965e04dc
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-02-02 16:00:12 -0500

    PP stuff

---
+++ Assets/Core/Scripts/PinapplePayload/PPLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPLogic : MonoBehaviour
+{
+    public List<GameObject> players = new List<GameObject>();
+
+    public Payload payloadprefab;
+    public Payload payload;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    public void BeginGame()
+    {
+
+    }
+
+    public GameObject RandomPlayer()
+    {
+        int p = Random.Range(0, players.Capacity);
+        return players[p];
+    }
+}
