015193eda382ffef1b99a3fd39902c828a0da5a9
ProdigyElite49 <62075379+ProdigyElite49@users.noreply.github.com>
2021-02-05 15:04:34 -0500

    Added explosive effetiveness, changed weapons, added grenade launcher

--- Assets/Core/Scripts/Weapons/Weapon.cs
+++ Assets/Core/Scripts/Weapons/Weapon.cs
@@ -9,16 +9,20 @@ public class Weapon : Actor
 {
     private GameObject UI;
     private PlayerController playerReference;
+    private Ammo_UI_Script AmmoReference;
+
+    private bool isReloading = false;
+    private bool isCooling = false;
 
     [Header("Dependencies")] 
     //public GameObject weaponModel;
     public GameObject projectilePrefab;
     public Transform projectileSpawn;
-    public Ammo_UI_Script AmmoReference;
 
     [Header("Stats")]
     public float    reloadSpeed;
     public float    fireRate;
+    [Range(0f,100f)]
     public float    bulletSpread;
     public float    knockBackForce;
     public int      currentClip;
@@ -26,14 +30,47 @@ public class Weapon : Actor
     
     
     [Header("Info")]
-    public bool isReloading = false; 
-    public bool isCooling = false;
+    public bool isRapidFire = false;
 
     protected KeyCode reloadBinding = KeyCode.R;
 
+    protected virtual void Start()
+    {
+        Debug.Log("Start--");
+        setPlayerReference();
+        setUIObj();
+        setAmmoReference();
+
+
+        currentClip = clipSize;
+        AmmoReference.SetMagazine(currentClip);
+        AmmoReference.SetReserve(clipSize);
+    }
+    private void Update()
+    {
+        if (isRapidFire)
+        {
+            if (Input.GetMouseButton(0)) { Fire(); }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0)) { Fire(); }
+        }
+        if (Input.GetKeyDown(reloadBinding))
+        {
+            reload();
+        }
+    }
 
     public virtual void Fire() 
     {
+        if (isReloading || clipEmpty() || isCooling) { return; }
+
+        GameObject go = Instantiate(projectilePrefab,
+                                    projectileSpawn.position,
+                                    Quaternion.LookRotation(BulletSpread(projectileSpawn.forward))
+                                    );
+
         currentClip--;
         AmmoReference.SetMagazine(currentClip);
         KnockBack(-transform.parent.forward, knockBackForce);
@@ -43,14 +80,6 @@ public class Weapon : Actor
     public virtual void AltFire() 
     { 
         
-    }
-    
-    protected virtual void Start()
-    {
-        Debug.Log("Start--");
-        setPlayerReference();
-        setUIObj();
-        setAmmoReference();
     }
 
     public virtual bool clipEmpty() 
@@ -123,12 +152,11 @@ public class Weapon : Actor
         }
     }
 
-    protected Quaternion BulletSpread(Quaternion input)
+    protected Vector3 BulletSpread(Vector3 input)
     {
-        input.x += Random.Range(-bulletSpread, bulletSpread);
-        input.y += Random.Range(-bulletSpread, bulletSpread);
+        input += Random.insideUnitSphere * bulletSpread/100;
 
-        return input;
+        return input.normalized;
     }
     void KnockBack(Vector3 direction, float magnitude)
     {
