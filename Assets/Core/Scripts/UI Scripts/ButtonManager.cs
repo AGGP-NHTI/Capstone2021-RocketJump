ae828b33539ee80cc73e513738da2be6bd776bec
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-02-05 10:45:29 -0500

    menu update (again)

---
+++ Assets/Core/Scripts/UI Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public List<CustomButton> buttons = new List<CustomButton>();
+    public AudioSource source;
+    public AudioClip clip;
+    
+    void Start()
+    {
+        foreach (CustomButton i in buttons)
+        {
+            i.source = source;
+            i.clip = clip;
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
