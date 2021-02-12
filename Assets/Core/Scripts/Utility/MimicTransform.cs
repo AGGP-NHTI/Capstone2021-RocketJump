948764f72961c954b37056983b3cd09989247ede
hucota7 <hucota7@gmail.com>
2021-02-12 13:51:39 -0500

    child-like behavior script

---
+++ Assets/Core/Scripts/Utility/MimicTransform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimicTransform : MonoBehaviour
+{
+	public Transform target;
+
+    void LateUpdate()
+    {
+        if (target)
+		{
+			transform.position = target.position;
+			transform.rotation = target.rotation;
+		}
+    }
+}
