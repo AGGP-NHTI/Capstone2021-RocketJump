00911873164de7c8ceee4df4879ecefc04440cef
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-02-19 16:02:28 -0500

    character select

---
+++ Assets/Core/Scripts/UI Scripts/CharacterIcons.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterIcons : CustomButton
+{
+    public GameObject endpoint;
+    public GameObject bigPic;
+    Vector3 target;
+
+    Vector3 startpos;
+
+    public float speed = 20f;
+
+    Vector3 MoveDirection;
+
+    protected override void Start()
+    {
+        startpos = gameObject.transform.position;
+        target = startpos;
+    }
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        MoveDirection = (target - transform.position).normalized;
+
+        transform.position += (Time.fixedDeltaTime * MoveDirection * speed * GetDistanceTo(target) * 0.5f);
+    }
+
+    public override void OnHighlight()
+    {
+        bigPic.SetActive(true);
+        target = endpoint.transform.position;
+        base.OnHighlight();
+    }
+
+    public override void OnHighlightEnd()
+    {
+        bigPic.SetActive(false);
+        target = startpos;
+        base.OnHighlightEnd();
+    }
+
+    public float GetDistanceTo(Vector3 Other)
+    {
+        float distanceTo = (Other - transform.position).magnitude;
+
+        return distanceTo;
+    }
+}
