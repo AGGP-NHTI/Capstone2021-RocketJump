393169d5e959c8aa67dce7ceb474915b751cd9d3
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-02-19 14:47:11 -0500

    payload stuff

--- Assets/Core/Scripts/PinapplePayload/PPLogic.cs
+++ Assets/Core/Scripts/PinapplePayload/PPLogic.cs
@@ -12,16 +12,40 @@ public class PPLogic : NetworkedBehaviour
     public Payload payloadprefab;
     public Payload payload;
 
-   
-    
+    public NetworkingManager manager;
+
+    public float gracetime = 15f;
+
+    [ServerRPC(RequireOwnership = false)]
     public void BeginGame()
     {
+        foreach (NewPC p in FindObjectsOfType<NewPC>())
+        {
+            players.Add(p.gameObject);
+        }
 
+        GameObject go = Instantiate(payloadprefab.gameObject, new Vector3(0, 0, 0), Quaternion.identity);
+        NetworkedObject netObj = go.GetComponent<NetworkedObject>();
+
+        payload = go.GetComponent<Payload>();
+        payload.logic = this;
+        
+        StartCoroutine(GracePeriod());
+    }
+    
+    public IEnumerator GracePeriod()
+    {
+        yield return new WaitForSeconds(gracetime);
+        InvokeServerRpc(payload.ChoosePlayer);        
     }
 
     public GameObject RandomPlayer()
     {
         int p = Random.Range(0, players.Capacity);
+        while (players[p] != null)
+        {
+            p = Random.Range(0, players.Capacity);
+        }
         return players[p];
     }
 }
