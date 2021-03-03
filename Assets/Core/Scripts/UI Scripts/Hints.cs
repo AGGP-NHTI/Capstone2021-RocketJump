b651c5e18c38255a949e7c0aa87452d4a6840ccb
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-03-03 17:49:05 -0500

    hints

---
+++ Assets/Core/Scripts/UI Scripts/Hints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Hints : MonoBehaviour
+{
+    public List<string> hints = new List<string>();
+    public TextMeshProUGUI loadtext;
+    
+    void Start()
+    {
+        int i = Random.Range(0, hints.Capacity);
+        loadtext.text = hints[i];
+        //Debug.Log(hints[i]);
+    }
+
+}
