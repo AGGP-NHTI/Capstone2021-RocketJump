9e11b7f5d7c12adde6125ce930c46348c51220d8
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-03-05 11:50:22 -0500

    credits

---
+++ Assets/Core/Scripts/UI Scripts/CreditSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditSetup : MonoBehaviour
+{
+
+    public CreditHover ch;
+    public GameObject fact;
+   
+    void Start()
+    {
+        ch.Facts = fact;
+    }
+
+   
+}
--- Assets/Core/Scripts/UI Scripts/CustomButton.cs
+++ Assets/Core/Scripts/UI Scripts/CustomButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CustomButton : Button
-{
+{   
     public AudioSource source;
     public AudioClip clip;
     bool pressed = false;
