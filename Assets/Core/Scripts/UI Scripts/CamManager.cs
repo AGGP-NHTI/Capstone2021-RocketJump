bf3633778a4d51dd8a7970e3f9cb232320513abd
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-03-05 14:08:22 -0500

    more cam pans

--- Assets/Core/Scripts/UI Scripts/CamManager.cs
+++ Assets/Core/Scripts/UI Scripts/CamManager.cs
@@ -4,31 +4,71 @@ using UnityEngine;
 
 public class CamManager : MonoBehaviour
 {
+    [Header("Arena")]
     public List<CamPan> cams = new List<CamPan>();
+
+    [Header("Race Track")]
+    public List<CamPan> cams2 = new List<CamPan>();
     int index = 0;
+    public int map = 0;
 
     void Start()
     {
+        map = Random.Range(0, 2);
+        //
+        
         foreach (CamPan i in cams)
         {
             i.gameObject.SetActive(false);
         }
-        cams[0].gameObject.SetActive(true);
-    }
-   
-    public void NextCam()
-    {        
-        cams[index].EndCam();
-
-        if (index + 1 < cams.Capacity)
+        foreach (CamPan i in cams2)
         {
-            index++;
+            i.gameObject.SetActive(false);
+        }
+
+        if (map == 0)
+        {
+            cams[0].gameObject.SetActive(true);
         }
         else
         {
-            index = 0;
+            cams2[0].gameObject.SetActive(true);
         }
 
-        cams[index].gameObject.SetActive(true);
+    }
+   
+    public void NextCam()
+    {
+        if (map == 0)
+        {
+            cams[index].EndCam();
+
+            if (index + 1 < cams.Capacity)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+
+            cams[index].gameObject.SetActive(true);
+        }
+
+        else
+        {
+            cams2[index].EndCam();
+
+            if (index + 1 < cams2.Capacity)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+
+            cams2[index].gameObject.SetActive(true);
+        }
     }
 }
