274c6ae59d42b0e0103eab05a544d72acf539208
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-03-15 15:31:25 -0400

    ad board

---
+++ Assets/Core/Scripts/UI Scripts/Flicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flicker : MonoBehaviour
+{
+    public Image image;
+    public Color Solid;
+    public Color transp;
+    bool c;
+    
+    void FixedUpdate()
+    {
+        if (c)
+        {
+            image.color = Solid;
+            c = false;
+        }
+        else
+        {
+            image.color = transp;
+            c = true;
+        }
+    }
+}
