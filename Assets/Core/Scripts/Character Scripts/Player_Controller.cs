8b04e6038357d5798a6d7b6cededcfa251ea23c6
ProdigyElite49 <62075379+ProdigyElite49@users.noreply.github.com>
2021-03-15 16:30:22 -0400

    Owndership

--- Assets/Core/Scripts/Character Scripts/Player_Controller.cs
+++ Assets/Core/Scripts/Character Scripts/Player_Controller.cs
@@ -37,8 +37,8 @@ public class Player_Controller : Controller
         location += Vector3.up * 10;
         GameObject playerPawn = Instantiate(PlayerPawn, location, Quaternion.identity);
         NetworkedObject netObj = playerPawn.GetComponent<NetworkedObject>();
-        netObj.Spawn();
-        netObj.ChangeOwnership(OwnerClientId);
+
+        netObj.SpawnWithOwnership(OwnerClientId);
 
 
         PossessPawn(playerPawn, netObj.OwnerClientId, netObj.NetworkId);
