f9bf71181ba46255e9ea66b7d2a7def121d32692
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2021-03-16 16:25:12 -0400

    crowd

---
+++ Assets/Core/Scripts/Tothop.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tothop : MonoBehaviour
+{
+    public GameObject hopdistance;
+    Vector3 downpos;
+    Vector3 uppos;
+    public float mintime, maxtime;
+    float current = 0;
+    public float speed = 30f;
+    Vector3 MoveDirection;
+    Vector3 target;
+    bool goingup = false;
+    
+
+    void Start()
+    {
+        downpos = gameObject.transform.position;
+        uppos = hopdistance.transform.position;
+        randomize();
+    }
+
+    
+    void FixedUpdate()
+    {
+        current -= Time.fixedDeltaTime;
+
+        if (current < 0)
+        {
+            randomize();
+            goingup = true;
+           
+        }
+        if (goingup)
+        {
+            target = uppos;
+            if (GetDistanceTo(uppos) <= 0.3f)
+            {
+                goingup = false;
+            }
+        }
+        else
+        {
+            target = downpos;
+        }
+        MoveDirection = (target - transform.position).normalized;
+
+        transform.position += (Time.fixedDeltaTime * MoveDirection * speed);
+    }
+
+    public void randomize()
+    {
+        current = Random.Range(mintime, maxtime);
+    }
+
+    public float GetDistanceTo(Vector3 Other)
+    {
+        float distanceTo = (Other - transform.position).magnitude;
+
+        return distanceTo;
+    }
+}
