3920726f909d17958b36ad2de4cf1fd8171f1502
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2021-03-16 15:43:02 -0400

    who let the dogs out?

--- Assets/Core/Scripts/Menu Scripts/ServerSlotScript.cs
+++ Assets/Core/Scripts/Menu Scripts/ServerSlotScript.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using MLAPI;
+using MLAPI.Transports.UNET;
 
 public class ServerSlotScript : MonoBehaviour
 {
     public serverInfo_SO info;
     public TextMeshProUGUI serverName;
+    public TMP_Dropdown selectionDropdown;
 
     public void Start()
     {
@@ -15,5 +18,47 @@ public class ServerSlotScript : MonoBehaviour
         {
             serverName.text = info.serverName;
         }
+
+        NetworkingManager.Singleton.OnClientConnectedCallback += (obj) =>
+        {
+            if (NetworkingManager.Singleton.IsClient)
+            {
+                Debug.Log("Connected.");
+            }
+            else
+            {
+                Debug.Log("Client joined");
+            }
+
+        };
+
+    }
+
+    public void confirmSelection()
+    {
+
+        NetworkingManager.Singleton.gameObject.GetComponent<UnetTransport>().ConnectAddress = info.connectAddress;
+
+        int i;
+        int.TryParse(info.connectPort, out i);
+
+        NetworkingManager.Singleton.gameObject.GetComponent<UnetTransport>().ConnectPort = i;
+
+        NetworkingManager.Singleton.gameObject.GetComponent<UnetTransport>().MLAPIRelayAddress = info.relayAddress;
+
+        int l;
+        int.TryParse(info.relayPort, out l);
+        NetworkingManager.Singleton.gameObject.GetComponent<UnetTransport>().MLAPIRelayPort = l;
+
+
+        //StartCoroutine(TaskStatus(NetworkingManager.Singleton.StartClient()));
+    }
+
+    IEnumerator TaskStatus(MLAPI.Transports.Tasks.SocketTasks tasks)
+    {
+        Debug.Log($"Listening to {tasks.Tasks.Length} tasks . . .");
+        yield return new WaitUntil(() => tasks.IsDone);
+
+        Debug.Log(tasks.Tasks[0].SocketError);
     }
 }
