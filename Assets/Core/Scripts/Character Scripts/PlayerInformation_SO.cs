0d3089392f94e5540c51f57e7dccff571379a672
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2021-03-23 14:14:12 -0400

    things and stuff

--- Assets/Core/Scripts/Character Scripts/PlayerInformation_SO.cs
+++ Assets/Core/Scripts/Character Scripts/PlayerInformation_SO.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player Data", menuName = "ScriptableObjects/PlayerData", order = 2)]
 public class PlayerInformation_SO : ScriptableObject
 {
-
+    public string playerCharacter;
+    /*
+     * 
+     */
 }
