73703b462eadccde8698e4ada454e06e21e8f403
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2021-04-20 15:52:29 -0400

    race logic push 1

--- Assets/Core/Scripts/TrackManagerScripts/LapManager.cs
+++ Assets/Core/Scripts/TrackManagerScripts/LapManager.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LapManager : MonoBehaviour
 {
 
-    //I meant to name it positionManager. I mean, we are gonna need a lap manager at somepoint, so I'll just leave this here lmao
+    //A testing script
+
+    public List<PlayerPositionManager> playerPositions;
+    public bool compare = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPositions.Add(new PlayerPositionManager(new GameObject(), new PositionManager(), "test1"));
+        playerPositions.Add(new PlayerPositionManager(new GameObject(), new PositionManager(), "test2"));
+        playerPositions.Add(new PlayerPositionManager(new GameObject(), new PositionManager(), "test3"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(compare)
+        {
+            compare = false;
+            comparePlayerPositions();
+            
+        }
     }
+
+    public void comparePlayerPositions()
+    {
+        print("compare positions");
+
+        playerPositions = (playerPositions.OrderByDescending(p => p.lap).ThenByDescending(p => p.nodePosition)).ToList();
+
+    }
+
 }
