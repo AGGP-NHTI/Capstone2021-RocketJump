942bfa5192d10d16e4004e1b665cd62bd8564726
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2021-04-27 20:25:50 -0400

    Update PositionManager.cs

--- Assets/Core/Scripts/TrackManagerScripts/PositionManager.cs
+++ Assets/Core/Scripts/TrackManagerScripts/PositionManager.cs
@@ -98,11 +98,6 @@ public class PositionManager : MonoBehaviour
             playerNames[i] = playerPositions[i].name;
         }
 
-        for(int i = 0; i < playerNames.Length; i++)
-        {
-            print(playerNames[i]);
-        }
-
         PlayerInformation.controller.PNC.hostSendPlayerFinished(playerNames);
     }
 
